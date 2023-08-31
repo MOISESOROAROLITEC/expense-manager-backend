fc91ed974b5ced55afe457f1c67d3875b3c2395d
moisesoroarolitec <moisesoro@arolitec.com>
2023-08-31 17:46:17 +0000

    change server port fro 3000 to 4000

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-var cors = require('cors')
-
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
+let cors = require("cors");
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    transform: true,
-    transformOptions:{
-      enableImplicitConversion:true,
-    },
-  }));
-  app.useGlobalGuards()
-  app.use(cors())
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  app.useGlobalGuards();
+
+  app.use(cors());
+  await app.listen(4000);
 }
 bootstrap();
